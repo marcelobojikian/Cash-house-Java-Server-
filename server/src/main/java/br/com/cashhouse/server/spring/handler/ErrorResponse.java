ce61c6c7ebfaa00d48f025368bf8b9daa36e4fc2
marcelobojikian <marcelo.bojikian@gmail.com>
2020-04-06 00:02:27 +0100

    Fix security OWASP

--- server/src/main/java/br/com/cashhouse/server/spring/handler/ErrorResponse.java
+++ server/src/main/java/br/com/cashhouse/server/spring/handler/ErrorResponse.java
@@ -2,8 +2,6 @@ package br.com.cashhouse.server.spring.handler;
 
 import java.time.LocalDateTime;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
@@ -13,14 +11,12 @@ public class ErrorResponse {
 	
     private LocalDateTime timestamp;
 
-    private String path;
     private Integer status;
     private String error;
     private String message;
     
-	public ErrorResponse(HttpServletRequest request, HttpStatus status, String message) {
+	public ErrorResponse(HttpStatus status, String message) {
 		super();
-		this.path = request.getRequestURI().substring(request.getContextPath().length());
 		this.status = status.value();
 		this.error = status.getReasonPhrase();
 		this.message = message;
